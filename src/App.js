e2ed05ffef9e2b7273838e5e86fbdb66ac757260
David Cutting <davidcutting42@gmail.com>
2019-06-13 18:38:23 -0600

    Rearchitecture to use firebase as backend

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import createHistory from 'history/createBrowserHistory'
 import Nav from './App.json'
 
 import MyPantry from "./components/MyPantry";
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
 
 const history = createHistory();
 history.listen((location, action) => {
@@ -23,24 +26,27 @@ history.listen((location, action) => {
     }
 });
 
+const firebaseConfig = {
+    apiKey: "AIzaSyBP3lvakMyYri3P9dJnT_csrj4ksHOAtfk",
+    authDomain: "reactive-kitchen-manager.firebaseapp.com",
+    databaseURL: "https://reactive-kitchen-manager.firebaseio.com",
+    projectId: "reactive-kitchen-manager",
+    storageBucket: "reactive-kitchen-manager.appspot.com",
+    messagingSenderId: "602819037461",
+    appId: "1:602819037461:web:658ccc77b403162b"
+};
+
+firebase.initializeApp(firebaseConfig);
+let db =firebase.firestore();
+
+
+
 class App extends Component {
-    state = {users: []}
-
-    componentDidMount() {
-        fetch('/users')
-            .then(res => res.json())
-            .then(users => this.setState({ users }));
-    }
-
     render() {
         return (
             <Router history={history}>
                 <div className="App">
                     <InterfaceFramework>
-                        {/*<h1>Users</h1>*/}
-                        {/*{this.state.users.map(user =>*/}
-                        {/*    <div key={user.id}>{user.username}</div>*/}
-                        {/*)}*/}
                         <Route path="/my-pantry" component={MyPantry}/>
                     </InterfaceFramework>
                 </div>
